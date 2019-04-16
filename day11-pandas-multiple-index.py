8192d28396f2347c572c84b1dcb96fd229a51f0c
greenmac <alwaysmac@msn.com>
2019-04-16 15:47:59 +0800

    day11-pandas-multiple-index

--- day11-pandas-multiple-index.py
+++ day11-pandas-multiple-index.py
@@ -1,1 +1,21 @@
 # https://ithelp.ithome.com.tw/articles/10194235
+import pandas as pd
+
+# bigmac = pd.read_csv('./big-mac-raw-index.csv')
+# bigmac.set_index(keys=['date', 'name'], inplace=True)
+# # print(bigmac)
+# # print(bigmac.index.names)
+# print(type(bigmac.index))
+
+# bigmac = pd.read_csv('./big-mac-raw-index.csv', parse_dates=['date'], index_col=['date', 'name'])
+# bigmac.sort_index(inplace=True)
+# print(bigmac.head(10))
+# print(bigmac.index.get_level_values(0)) # [0] = ['date']
+# print(bigmac.index.get_level_values('date')) # [0] = ['date']
+# print(bigmac.index.get_level_values(1)) # [1] = ['name']
+# print(bigmac.index.get_level_values('name')) # [1] = ['name']
+
+bigmac = pd.read_csv('./big-mac-raw-index.csv', parse_dates=['date'], index_col=['date', 'name'])
+bigmac.sort_index(inplace=True)
+bigmac.index.set_names(['day', 'location'], inplace=True)
+print(bigmac.head())
