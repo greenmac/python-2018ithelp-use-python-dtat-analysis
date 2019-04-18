7d38a52bfafe3b01898dd13f59158d4558757557
greenmac <alwaysmac@msn.com>
2019-04-18 10:58:12 +0800

    day13-pandas-string-02

--- day13-pandas-string-02.py
+++ day13-pandas-string-02.py
@@ -1,1 +1,15 @@
 # https://ithelp.ithome.com.tw/articles/10195183
+
+# print("HELLO WORLD".lower()) # lower()讓字串全部變成小寫
+# print("hello world".upper()) # upper() 讓字串全部變成大寫
+# print("hello world".title()) # title() 讓字串第一個字為大寫
+# print(len("hello world")) # len() 得到字串長度
+
+import pandas as pd
+
+bigmac = pd.read_csv('big-mac-raw-index.csv')
+bigmac['name'] = bigmac['name'].astype('category')
+bigmac['iso_a3'] = bigmac['iso_a3'].str.title()
+# bigmac["name"] = bigmac["name"].str.lower().str.contains("taiwan") # bool
+bigmac['name'] = bigmac['name'].str.split(' ')
+print(bigmac)
