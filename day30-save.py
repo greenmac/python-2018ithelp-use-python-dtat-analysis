622420c8eae6703a352205237f1facd83cc4fa3e
greenmac <alwaysmac@msn.com>
2019-04-25 14:36:16 +0800

    day30-save

--- day30-save.py
+++ day30-save.py
@@ -1,1 +1,23 @@
 # https://ithelp.ithome.com.tw/articles/10197575
+from sklearn import svm
+from sklearn import datasets
+from sklearn.externals import joblib
+
+
+clf = svm.SVC()
+iris = datasets.load_iris()
+X, y = iris['data'], iris['target']
+clf.fit(X, y)
+"""
+save
+"""
+# joblib.dump(clf, './data/clf.pkl')
+
+"""
+load
+"""
+clf2 = joblib.load('./data/clf.pkl')
+clf2_predict = clf2.predict(X[0:100])
+
+# print(X[0:10])
+print(clf2_predict)
